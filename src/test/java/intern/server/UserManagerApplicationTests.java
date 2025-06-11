542cbce562fc79164b1aa3d13b1d915fc3da407d
NguyenDuoc3006 <duocnhph50257@gmail.com>
2025-06-11 09:30:59 +0700

    first commit

---
+++ src/test/java/intern/server/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package intern.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
